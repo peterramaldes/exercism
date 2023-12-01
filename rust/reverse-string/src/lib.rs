7de90695917135937ce53fd3344238ea8da56c2b
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2023-12-01 06:00:57 -0300

    Easy way to reverse string in rust.

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,9 +1,3 @@
 pub fn reverse(input: &str) -> String {
-    if input.len() == 0 { return String::from("") }
-    let mut result = String::new(); 
-    for i in (0..input.chars().count()).rev() {
-       let letter = input.chars().nth(i).expect("retrieve letter from index");
-       result.push(letter);
-    }
-    result
+    input.chars().rev().collect()
 }
